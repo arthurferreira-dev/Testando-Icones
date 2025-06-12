6afc6339cbe816f66a6149b97dcc5ce123ddd298
Arthur Ferreira <tutudacosta996@gmail.com>
2025-06-11 22:02:09 -0300

    alpha v2.1

---
+++ js/zoom-icons.js
@@ -0,0 +1,67 @@
+function About() {
+    let icon = document.getElementById('about')
+    let button = document.getElementById('btn-dowloand')
+    let mark = document.getElementById('x-mark')
+
+    icon.style.display = 'block'
+    icon.style.margin = 'auto'
+    icon.style.marginBottom = '12px'
+    icon.style.height = '84px'
+    icon.style.width = '84px'
+
+    button.style.display = 'block'
+    button.style.margin = 'auto'
+
+    mark.style.display = 'block'
+}
+
+function Return() {
+    let icon = document.getElementById('about')
+    let button = document.getElementById('btn-dowloand')
+    let mark = document.getElementById('x-mark')
+
+    icon.style.display = 'inline'
+    icon.style.margin = '8px'
+    icon.style.marginBottom = '8px'
+    icon.style.height = '42px'
+    icon.style.width = '42px'
+
+    button.style.display = 'none'
+    button.style.margin = ''
+
+    mark.style.display = 'none'
+}
+
+function Alarm() {
+    let icon = document.getElementById('alarm-clock')
+    let button = document.getElementById('btn-dowloand')
+    let mark = document.getElementById('x-mark')
+
+    icon.style.display = 'block'
+    icon.style.margin = 'auto'
+    icon.style.marginBottom = '12px'
+    icon.style.height = '84px'
+    icon.style.width = '84px'
+
+    button.style.display = 'block'
+    button.style.margin = 'auto'
+
+    mark.style.display = 'block'
+}
+
+function Return2() {
+    let icon = document.getElementById('alarm-clock')
+    let button = document.getElementById('btn-dowloand')
+    let mark = document.getElementById('x-mark')
+
+    icon.style.display = 'inline'
+    icon.style.margin = '8px'
+    icon.style.marginBottom = '8px'
+    icon.style.height = '42px'
+    icon.style.width = '42px'
+
+    button.style.display = 'none'
+    button.style.margin = ''
+
+    mark.style.display = 'none'
+}
